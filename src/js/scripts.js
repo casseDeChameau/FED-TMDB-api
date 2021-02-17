9dfe8986f98cf9a9064dfe27aae3d8eff7c31e29
casseDeChameau <48219695+casseDeChameau@users.noreply.github.com>
2021-02-17 09:16:03 +0100

    mise en page de base

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,11 @@
 
 let xhr = new XMLHttpRequest();
 let myKey = '4250bfd9b87434db15ff28ef57ec3c08';
-let tmdbApiUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=' + myKey + '&language=en-US&page-1';
+let upcomingUrl = 'https://api.themoviedb.org/3/movie/upcoming?api_key=4250bfd9b87434db15ff28ef57ec3c08&language=en-US&page-1';
+let trendingUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=4250bfd9b87434db15ff28ef57ec3c08&language=en-US&page=1';
+// let filmUrl = 
+// let Url = documentary
+// let Url = series
 let btn = document.querySelector('button');
 let searchRequest = document.querySelector('[name="search-movie"]');
 
@@ -25,6 +29,7 @@ function newFrame(movie) {
     movieWrap.appendChild(curDiv);
 }
 
+
 function myAjaxFunction() {
     if (this.status == 200 && this.readyState == 4) {
         let monJSON = JSON.parse(this.responseText);
@@ -34,8 +39,15 @@ function myAjaxFunction() {
     }
 }
 
+// function myCallAjax() {
+//     if (curentTarget) {
+//         xhr.open('GET', tmdbApiUrl, true);
+//     }
+//     xhr.send();
+// }
+
 function myCallAjax() {
-    xhr.open('GET', tmdbApiUrl, true);
+    xhr.open('GET', upcomingUrl, true);
     xhr.send();
 }
 
@@ -45,13 +57,22 @@ function myCallAjax() {
 
 xhr.addEventListener('readystatechange', myAjaxFunction);
 window.addEventListener('load', myCallAjax);
+
 // btn.addEventListener('click', myCallAjax);
 // searchRequest.addEventListener('focusin', watchForSearch);
 
 
-// for (let i = 0; i < monJSON.results.length; i++) {
-//     console.log(i, monJSON.results[i].adult, );
-// }
-// monJSON.results.forEach(function(movie, i) {
-//     console.log(movie.original_language);
-// });
+
+/* 
+for (let i = 0; i < monJSON.results.length; i++) {
+    console.log(i, monJSON.results[i].adult, );
+}
+
+monJSON.results.forEach(function(movie, i) {
+    console.log(movie.original_language);
+});
+
+monJSON.results.forEach((movie) => {
+   newFrame(movie);
+}); 
+*/
